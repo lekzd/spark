77812da3cd1fcdfdfa07eda55d81d8d195290285
Aleksandr Korotaev <lekzd@mail.ru>
2026-04-30 16:58:25 +0300

    fix: build

--- src/SplatPager.ts
+++ src/SplatPager.ts
@@ -828,44 +828,53 @@ export class SplatPager {
 
     const packedTexture = this.packedTexture.value;
     packedTexture.dispose();
-    this.packedTexture.value = SplatPager.emptyPackedTexture;
+    this.packedTexture.value.source.data = null;
+    // this.packedTexture.value = SplatPager.emptyPackedTexture;
 
     if (this.extTexture.value !== SplatPager.emptyExtTexture) {
       this.extTexture.value.dispose();
+      this.extTexture.value.source.data = null;
     }
-    this.extTexture.value = SplatPager.emptyExtTexture;
+    // this.extTexture.value = SplatPager.emptyExtTexture;
 
     if (!this.extSplats) {
       if (this.sh1Texture.value !== SplatPager.emptySh1Texture) {
         this.sh1Texture.value.dispose();
+        this.sh1Texture.value.source.data = null;
       }
-      this.sh1Texture.value = SplatPager.emptySh1Texture;
+      // this.sh1Texture.value = SplatPager.emptySh1Texture;
       if (this.sh2Texture.value !== SplatPager.emptySh2Texture) {
         this.sh2Texture.value.dispose();
+        this.sh2Texture.value.source.data = null;
       }
-      this.sh2Texture.value = SplatPager.emptySh2Texture;
+      // this.sh2Texture.value = SplatPager.emptySh2Texture;
       if (this.sh3Texture.value !== SplatPager.emptySh3Texture) {
         this.sh3Texture.value.dispose();
+        this.sh3Texture.value.source.data = null;
       }
-      this.sh3Texture.value = SplatPager.emptySh3Texture;
-      this.sh3TextureB.value = SplatPager.emptyExtSh3BTexture;
+      // this.sh3Texture.value = SplatPager.emptySh3Texture;
+      // this.sh3TextureB.value = SplatPager.emptyExtSh3BTexture;
     } else {
       if (this.sh1Texture.value !== SplatPager.emptyExtSh1Texture) {
         this.sh1Texture.value.dispose();
+        this.sh1Texture.value.source.data = null;
       }
-      this.sh1Texture.value = SplatPager.emptyExtSh1Texture;
+      // this.sh1Texture.value = SplatPager.emptyExtSh1Texture;
       if (this.sh2Texture.value !== SplatPager.emptyExtSh2Texture) {
         this.sh2Texture.value.dispose();
+        this.sh2Texture.value.source.data = null;
       }
-      this.sh2Texture.value = SplatPager.emptyExtSh2Texture;
+      // this.sh2Texture.value = SplatPager.emptyExtSh2Texture;
       if (this.sh3Texture.value !== SplatPager.emptyExtSh3Texture) {
         this.sh3Texture.value.dispose();
+        this.sh3Texture.value.source.data = null;
       }
-      this.sh3Texture.value = SplatPager.emptyExtSh3Texture;
+      // this.sh3Texture.value = SplatPager.emptyExtSh3Texture;
       if (this.sh3TextureB.value !== SplatPager.emptyExtSh3BTexture) {
         this.sh3TextureB.value.dispose();
+        this.sh3TextureB.value.source.data = null;
       }
-      this.sh3TextureB.value = SplatPager.emptyExtSh3BTexture;
+      // this.sh3TextureB.value = SplatPager.emptyExtSh3BTexture;
     }
   }
 
